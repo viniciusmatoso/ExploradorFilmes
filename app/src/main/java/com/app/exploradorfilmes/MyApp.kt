d55f019aa99c901c5453378566400dd81073396a
VINÖCIUS\marcu <marcus_matoso@hotmail.com>
2023-10-18 01:10:48 -0300

    testando chamada log

--- app/src/main/java/com/app/exploradorfilmes/MyApp.kt
+++ app/src/main/java/com/app/exploradorfilmes/MyApp.kt
@@ -5,13 +5,16 @@ import com.app.exploradorfilmes.di.apiModule
 import com.app.exploradorfilmes.di.retrofitModule
 import com.app.exploradorfilmes.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
+            androidLogger(Level.NONE)
             androidContext(this@MyApp)
             modules(
                 retrofitModule,
--- app/src/main/java/com/app/exploradorfilmes/presentation/MainActivity.kt
+++ app/src/main/java/com/app/exploradorfilmes/presentation/MainActivity.kt
@@ -2,11 +2,38 @@ package com.app.exploradorfilmes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.app.exploradorfilmes.R
+import android.util.Log
+import com.app.exploradorfilmes.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.call.setOnClickListener{
+            callPopularMoveis()
+        }
+
+        viewModel.popularMovie.observe(this) { popularMovie ->
+            popularMovie.forEach {
+                Log.i("PopularMovie", it.title)
+            }
+        }
+
+    }
+
+    private fun callPopularMoveis(){
+        GlobalScope.launch {
+            viewModel.getPopularMoveis()
+        }
     }
 }
